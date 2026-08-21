1b34ca9e3183c8598fe39ce33cc453d7bce91642
Christopher <christopherloayza21@gmail.com>
2026-08-21 18:49:29 -0500

    Secure .NET API configuration

---
+++ GasesIndustriales/GasesIndustriales.Api/Controllers/ClientesController.cs
@@ -0,0 +1,26 @@
+﻿using GasesIndustriales.Api.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GasesIndustriales.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ClientesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetClientes()
+        {
+            var clientes = await _context.Clientes.ToListAsync();
+
+            return Ok(clientes);
+        }
+    }
+}
---
+++ GasesIndustriales/GasesIndustriales.Api/Data/AppDbContext.cs
@@ -0,0 +1,52 @@
+﻿using GasesIndustriales.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GasesIndustriales.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>(entity =>
+            {
+                entity.ToTable("cliente");
+
+                entity.HasKey(e => e.IdCliente);
+
+                entity.Property(e => e.IdCliente)
+                    .HasColumnName("id_cliente");
+
+                entity.Property(e => e.RazonSocial)
+                    .HasColumnName("razon_social");
+
+                entity.Property(e => e.Ruc)
+                    .HasColumnName("ruc");
+
+                entity.Property(e => e.Telefono)
+                    .HasColumnName("telefono");
+
+                entity.Property(e => e.Direccion)
+                    .HasColumnName("direccion");
+
+                entity.Property(e => e.IdZona)
+                    .HasColumnName("id_zona");
+
+                entity.Property(e => e.TipoCliente)
+                    .HasColumnName("tipo_cliente");
+
+                entity.Property(e => e.RequiereGarantia)
+                    .HasColumnName("requiere_garantia");
+
+                entity.Property(e => e.Activo)
+                    .HasColumnName("activo");
+            });
+        }
+    }
+}
