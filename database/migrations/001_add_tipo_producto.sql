8bdce96e749c51fc929c11ed6cb9561b625af78d
Christopher <christopherloayza21@gmail.com>
2026-08-21 20:11:40 -0500

    Move dashboard system to separate repository

--- database/migrations/001_add_tipo_producto.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE producto
-ADD COLUMN IF NOT EXISTS tipo_producto VARCHAR(20) NOT NULL DEFAULT 'GAS';
-
-ALTER TABLE producto
-DROP CONSTRAINT IF EXISTS chk_producto_tipo;
-
-ALTER TABLE producto
-ADD CONSTRAINT chk_producto_tipo
-CHECK (tipo_producto IN ('GAS', 'EQUIPO', 'INSUMO', 'SERVICIO'));
